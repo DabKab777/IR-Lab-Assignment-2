ff02cf795e3b6caf0c74844833c6a82c1b0fe7f7
MorneKruger1 <125844917+MorneKruger1@users.noreply.github.com>
2023-10-24 13:27:59 +1100

    NEW THOR FILES

---
+++ UPDATED_THOR_FILES/@Thor/Thor.m
@@ -0,0 +1,49 @@
+classdef Thor < RobotBaseClassNoColour
+    %% Thor 3D Printable open Source robot model
+    %
+    % WARNING: This model has been created by UTS students in the subject
+    % 41013. No guarentee is made about the accuracy or correctness of the
+    % of the DH parameters of the accompanying ply files. Do not assume
+    % that this matches the real robot!
+
+    properties(Access = public)   
+        plyFileNameStem = 'Thor';
+    end
+    
+    methods
+%% Constructor
+        function self = Thor(baseTr,useTool,toolFilename)
+            if nargin < 3
+                if nargin == 2
+                    error('If you set useTool you must pass in the toolFilename as well');
+                elseif nargin == 0 % Nothing passed
+                    baseTr = transl(0,0,0);  
+                end             
+            else % All passed in 
+                self.useTool = useTool;
+                toolTrData = load([toolFilename,'.mat']);
+                self.toolTr = toolTrData.tool;
+                self.toolFilename = [toolFilename,'.ply'];
+            end
+          
+            self.CreateModel();
+			self.model.base = self.model.base.T * baseTr;
+            self.model.tool = self.toolTr;
+            self.PlotAndColourRobot();
+
+            drawnow
+        end
+
+%% CreateModel
+        function CreateModel(self)
+            link(1) = Link('d',0.202,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]), 'offset',0);
+            link(2) = Link('d',0,'a',0.16,'alpha',0,'qlim', deg2rad([0 180]), 'offset',0);
+            link(3) = Link('d',0,'a',0,'alpha',-pi/2,'qlim', deg2rad([0 -180]), 'offset', 0);
+            link(4) = Link('d',0.195,'a',0,'alpha',pi/2,'qlim',deg2rad([-180 180]),'offset', 0);
+            link(5) = Link('d',0,'a',0.06715,'alpha',-pi/2,'qlim',deg2rad([0 180]), 'offset',0);
+            %%link(6) = Link('d',0.0819,'a',0,'alpha',0,'qlim',deg2rad([-360,360]), 'offset', 0);
+             
+            self.model = SerialLink(link,'name',self.name);
+        end      
+    end
+end
