a1d301e453f00c97ac4cdd009ac4e590afebecd3
MorneKruger1 <125844917+MorneKruger1@users.noreply.github.com>
2023-10-24 19:26:26 +1100

    more Code for moving cans

--- UPDATED_THOR_FILES/TestCode/TestModel.m
+++ UPDATED_THOR_FILES/TestCode/TestModel.m
@@ -16,7 +16,7 @@ EmergencyStop = PlaceObject('emergencyStopWallMounted.ply', [1,2.14,1.5]);
 InductedWorker = PlaceObject('personMaleConstruction.ply', [-3,0, 0]);
 UninductedWorker = PlaceObject('personMaleCasual.ply', [-2.5,-2, 0]);
 
-RedCan = PlaceObject('RedCanV2.ply', [1 ,0, 0.6]);
+RedCan = PlaceObject('RedCan.ply', [1 ,0, 0.6]);
 % workspace = [-2 2  -2 2  0 5 ];
 
 robot_pos = r.model.getpos();
